989d7d3ad2ae7092add70e1bdf6ed7bbe99ba278
Ainslee Shuemake <ainsleeshuemake@campus-038-158.ucdavis.edu>
2024-10-08 14:01:56 -0700

    Week 2 homeowrk

---
+++ Week2_Homework.R
@@ -0,0 +1,21 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+#this is how to identify the NA values in a vector
+is.na(hw2)
+
+#this is how to remove the NA values from your vector
+hw2[!is.na(hw2)]
+
+hw2[!is.na(hw2)] [hw2[!is.na(hw2)]>= 14 & hw2[!is.na(hw2)]<=38] 
+prob1 <- hw2[!is.na(hw2)] [hw2[!is.na(hw2)]>= 14 & hw2[!is.na(hw2)]<=38]
+
+times3<-prob1*3
+times3 #question2
+
+plus10<- times3 + 10
+plus10 #question2
+
+plus10[c(TRUE, FALSE)]
