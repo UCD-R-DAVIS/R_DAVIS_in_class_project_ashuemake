4bf85bc83dac8ccb742a2b1cc9b6c05508e068db
Ainslee Shuemake <ainsleeshuemake@MacBookAir.attlocal.net>
2024-10-24 07:59:27 -0700

    Week 3 homework in class notes

--- homework/Week_3_homework.R
+++ homework/Week_3_homework.R
@@ -12,3 +12,17 @@ surveys_base
 
 
 levels(surveys_base$species_id)
+
+
+## hw 3 review
+
+# Homework 3 Review -----
+#Load your survey data frame with the read.csv() function. Create a new data frame called surveys_base with only the species_id, the weight, and the plot_type columns. Have this data frame only be the first 5,000 rows. 
+
+
+#Convert both species_id and plot_type to factors. Remove all rows where there is an NA in the weight column. 
+
+
+#Explore these variables and try to explain why a factor is different from a character. Why might we want to use factors? Can you think of any examples?
+
+#CHALLENGE: Create a second data frame called challenge_base that only consists of individuals from your surveys_base data frame with weights greater than 150g.
